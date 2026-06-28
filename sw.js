87304ecc4aa49c0d8e0e4e3a2d98d77a00ad6df5
esther1935 <esd2011@naver.com>
2026-06-28 22:18:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "siel-aac-app-sielAdminFinal20260628";
-const IMAGE_CACHE = "siel-aac-image-cache-v17";
+const CACHE_NAME = "siel-aac-app-sielRoleAdminFinal20260628";
+const IMAGE_CACHE = "siel-aac-image-cache-roleadmin";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=sielAdminFinal20260628",
-  "./app.js?v=sielAdminFinal20260628",
-  "./manifest.webmanifest?v=sielAdminFinal20260628"
+  "./styles.css?v=sielRoleAdminFinal20260628",
+  "./app.js?v=sielRoleAdminFinal20260628",
+  "./manifest.webmanifest?v=sielRoleAdminFinal20260628"
 ];
 
 self.addEventListener("install", (event) => {
